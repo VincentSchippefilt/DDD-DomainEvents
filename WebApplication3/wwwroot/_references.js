c1041ec05753e0e679144fd2955658558b994b80
Vincent Schippefilt <vincent.schippefilt@gfi.be>
2015-08-15 15:37:18 +0200

    commit including the generation of the durandal app using yo durandal in the wwwroot folder. this includes the duplicate of the package manager config files, that need to be merged

---
+++ WebApplication3/wwwroot/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../gulpfile.js" />
+/// <reference path="app/main.js" />
+/// <reference path="app/viewmodels/home.js" />
+/// <reference path="app/viewmodels/shell.js" />
+/// <reference path="gruntfile.js" />
+/// <reference path="js/site.js" />
